ce859d501968c78cb0d2236df130c60bb5278aa4
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-08-05 17:29:52 -0400

    fix(browse): Use a simpler approach for representing paths responsively

---
+++ frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
@@ -0,0 +1,43 @@
+import { useAreAnyTruncated } from "metabase/hooks/use-is-truncated";
+import { Tooltip } from "metabase/ui";
+
+import S from "./EllipsifiedPath.module.css";
+
+type EllipsifiedPathProps = { items: string[]; tooltip: string };
+
+export const EllipsifiedPath = ({ items, tooltip }: EllipsifiedPathProps) => {
+  const { areAnyTruncated, ref } = useAreAnyTruncated<HTMLDivElement>();
+
+  const isTooltipEnabled = areAnyTruncated;
+  return (
+    <Tooltip label={tooltip} disabled={!isTooltipEnabled} multiline maw="20rem">
+      <div>
+        <div className={S.path}>
+          {items.map((item, index) => {
+            const key = `${item}${index}`;
+            return (
+              <>
+                <div
+                  key={key}
+                  ref={(el: HTMLDivElement | null) => {
+                    if (ref && typeof ref !== "function" && ref.current && el) {
+                      ref.current.set(key, el);
+                    }
+                  }}
+                  className={S.pathItem}
+                >
+                  {item}
+                </div>
+                {index < items.length - 1 && (
+                  <div key={`separator${index}`} className={S.pathSeparator}>
+                    /
+                  </div>
+                )}
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </Tooltip>
+  );
+};
