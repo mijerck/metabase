269d2d2a951cb7a99c70bb48b30e16047dbcd331
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-08-06 10:39:52 -0400

    Add doc

--- frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
+++ frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
@@ -5,9 +5,19 @@ import S from "./EllipsifiedPath.module.css";
 
 type EllipsifiedPathProps = { items: string[]; tooltip: string };
 
+/**
+ * Displays a path such as "Collection / Subcollection / Subsubcollection /
+ * Parent Collection".
+ *
+ * If the path is too long to fit, some items may be truncated, like this:
+ * "Collection / Subcollec... / Subsub... / Parent Collection".
+ *
+ * The first and last item are never truncated.
+ *
+ * A tooltip is shown if any items are truncated.
+ */
 export const EllipsifiedPath = ({ items, tooltip }: EllipsifiedPathProps) => {
   const { areAnyTruncated, ref } = useAreAnyTruncated<HTMLDivElement>();
-
   return (
     <Tooltip label={tooltip} disabled={!areAnyTruncated} multiline maw="20rem">
       <div>
