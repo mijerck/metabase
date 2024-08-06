81153d8e1aab587fcdfd18b61ec3e606549c522d
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-08-06 10:54:05 -0400

    Remove extra div

--- frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
+++ frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
@@ -20,32 +20,30 @@ export const EllipsifiedPath = ({ items, tooltip }: EllipsifiedPathProps) => {
   const { areAnyTruncated, ref } = useAreAnyTruncated<HTMLDivElement>();
   return (
     <Tooltip label={tooltip} disabled={!areAnyTruncated} multiline maw="20rem">
-      <div>
-        <div className={S.path}>
-          {items.map((item, index) => {
-            const key = `${item}${index}`;
-            return (
-              <>
-                <div
-                  key={key}
-                  ref={(el: HTMLDivElement | null) => {
-                    if (ref && typeof ref !== "function" && ref.current && el) {
-                      ref.current.set(key, el);
-                    }
-                  }}
-                  className={S.pathItem}
-                >
-                  {item}
+      <div className={S.path}>
+        {items.map((item, index) => {
+          const key = `${item}${index}`;
+          return (
+            <>
+              <div
+                key={key}
+                ref={(el: HTMLDivElement | null) => {
+                  if (ref && typeof ref !== "function" && ref.current && el) {
+                    ref.current.set(key, el);
+                  }
+                }}
+                className={S.pathItem}
+              >
+                {item}
+              </div>
+              {index < items.length - 1 && (
+                <div key={`separator${index}`} className={S.pathSeparator}>
+                  /
                 </div>
-                {index < items.length - 1 && (
-                  <div key={`separator${index}`} className={S.pathSeparator}>
-                    /
-                  </div>
-                )}
-              </>
-            );
-          })}
-        </div>
+              )}
+            </>
+          );
+        })}
       </div>
     </Tooltip>
   );
