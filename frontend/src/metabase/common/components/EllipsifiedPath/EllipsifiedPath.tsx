6b5a481008bc1e46c5671055f041d0e6abfebf00
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-08-07 10:36:10 -0400

    Simplify - remove ellipsification of path items

--- frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
+++ frontend/src/metabase/common/components/EllipsifiedPath/EllipsifiedPath.tsx
@@ -1,7 +1,3 @@
-import cx from "classnames";
-import { useMemo, useRef } from "react";
-
-import { useIsResizing } from "metabase/common/hooks/use-is-resizing";
 import { useAreAnyTruncated } from "metabase/hooks/use-is-truncated";
 import { Tooltip } from "metabase/ui";
 
@@ -22,23 +18,10 @@ type EllipsifiedPathProps = { items: string[]; tooltip: string };
  */
 export const EllipsifiedPath = ({ items, tooltip }: EllipsifiedPathProps) => {
   const { areAnyTruncated, ref } = useAreAnyTruncated<HTMLDivElement>();
-  const path = useRef<HTMLDivElement | null>(null);
-  const resizing = useIsResizing(path.current);
-  const smallItemCount = useMemo(
-    () =>
-      [...ref.current.values()].slice(0, -1).filter(el => el.offsetWidth < 25)
-        .length,
-    [ref],
-  );
 
   return (
-    <Tooltip
-      label={tooltip}
-      disabled={!areAnyTruncated && smallItemCount === 0}
-      multiline
-      maw="20rem"
-    >
-      <div className={S.path} ref={path}>
+    <Tooltip label={tooltip} disabled={!areAnyTruncated} multiline maw="20rem">
+      <div className={S.path}>
         {items.length > 1 && (
           <div className={S.dots}>
             … <div className={S.slash}>/</div>
@@ -46,15 +29,12 @@ export const EllipsifiedPath = ({ items, tooltip }: EllipsifiedPathProps) => {
         )}
         {items.map((item, index) => {
           const key = `${item}${index}`;
-          const div = ref.current.get(key);
           return (
             <>
               <div
                 key={key}
                 ref={el => el && ref.current.set(key, el)}
-                className={cx(S.item, {
-                  [S.small]: !resizing && div && div.offsetWidth < 25,
-                })}
+                className={S.item}
               >
                 {item}
               </div>
